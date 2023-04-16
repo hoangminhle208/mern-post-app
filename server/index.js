e4931a869ad8483ad4185a11638aa42e8dacfbbc
hoangminhle208 <hoanganhkks01@gmail.com>
2023-04-16 17:42:45 +0700

    app

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import posts from './router/post.js';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = process.env.port || 5000;
+const URI = 'mongodb+srv://admin:C4o9JGNrchVaJ70H@cluster0.gjq4fso.mongodb.net/?retryWrites=true&w=majority'
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true,limit:'30mb'}));
+app.use(cors());
+
+app.use('/posts',posts);
+
+mongoose.connect(URI,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=>{
+        console.log('Connected to DB');
+        app.listen(PORT,()=>{
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err)=>{
+        console.log('err',err);
+    })
